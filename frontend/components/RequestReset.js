d1e9358ef8d3696ff936a49f79bf8ed2c78a3010
Ethan Glover <hello@ethang.email>
2021-02-17 20:18:41 -0600

    Password reset request: 	modified:   backend/keystone.ts 	new file:   frontend/components/RequestReset.js 	modified:   frontend/components/SignUp.js 	modified:   frontend/pages/signin.js

---
+++ frontend/components/RequestReset.js
@@ -0,0 +1,59 @@
+import { gql, useMutation } from '@apollo/client';
+import Form from '../styles/Form';
+import useForm from '../lib/useForm';
+import { CURRENT_USER_QUERY } from './User';
+import DisplayError from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink(email: $email) {
+      code
+      message
+    }
+  }
+`;
+
+export default function RequestReset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+  });
+
+  const [signUp, { data, loading, error }] = useMutation(
+    REQUEST_RESET_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await signUp().catch(console.error);
+    resetForm();
+  }
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Request Password Reset</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>Success! Check your email for a link.</p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Request Reset</button>
+      </fieldset>
+    </Form>
+  );
+}
+
+RequestReset.propTypes = {};
