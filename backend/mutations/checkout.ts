b378e70e3c759c85a2af0eb6fef633a98c974207
Ethan Glover <hello@ethang.email>
2021-02-20 19:25:10 -0600

    Stripe checkout setup: 	new file:   backend/lib/stripe.ts 	modified:   backend/mutations/addToCart.ts 	new file:   backend/mutations/checkout.ts 	modified:   backend/mutations/index.ts

---
+++ backend/mutations/checkout.ts
@@ -0,0 +1,64 @@
+/* eslint-disable */
+import { KeystoneContext, SessionStore } from '@keystone-next/types';
+import {CartItemCreateInput, OrderCreateInput} from './../.keystone/schema-types'
+import stripeConfig from "../lib/stripe";
+
+const graphql = String.raw;
+
+interface Arguments {
+    token: string,
+}
+
+async function checkout(
+    root: any,
+    { token }: Arguments,
+    context: KeystoneContext
+): Promise<OrderCreateInput> {
+    const userId = context.session.itemId;
+    if(!userId) {
+        throw new Error('Sorry! You must be signed in to create an order!');
+    }
+
+    const user = await context.lists.User.findOne({
+        where: {id: userId},
+        resolveFields: graphql`
+            id
+            name 
+            email
+            cart {
+                id
+                quantity
+                product {
+                    name
+                    price
+                    description
+                    id
+                    photo {
+                        id
+                        image {
+                            id
+                            publicUrlTransformed
+                        }
+                    }
+                }
+            }
+        `
+    })
+
+    const cartItems = user.cart.filter(cartItem => cartItem.product);
+    const amount = cartItems.reduce(function (tally: number, cartItem: CartItemCreateInput) {
+        return tally + cartItem.quantity * cartItem.product.price;
+    }, 0);
+
+    const charge = await stripeConfig.paymentIntents.create({
+        amount,
+        currency: 'USD',
+        confirm: true,
+        payment_method: token,
+    }).catch(err => {
+        console.log(err);
+        throw new Error(err.message);
+    })
+}
+
+export default checkout;
