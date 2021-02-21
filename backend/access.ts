af4537818d908eb156bdeddd40557190da419677
Ethan Glover <hello@ethang.email>
2021-02-21 12:43:58 -0600

    Permissions rules:: 	modified:   backend/access.ts 	modified:   backend/schemas/CartItem.ts 	modified:   backend/schemas/Order.ts 	modified:   backend/schemas/OrderItem.ts 	modified:   backend/schemas/Role.ts

--- backend/access.ts
+++ backend/access.ts
@@ -1,5 +1,5 @@
-import { ListAccessArgs } from './types';
 import { permissionsList } from './schemas/fields';
+import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
@@ -20,17 +20,48 @@ export const permissions = {
 
 export const rules = {
   canManageProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
     if (permissions.canManageProducts({ session })) {
       return true;
     }
-
     return { user: { id: session.itemId } };
   },
+  canOrder({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    return { user: { id: session.itemId } };
+  },
+  canManageOrderItems({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    return { order: { user: { id: session.itemId } } };
+  },
   canReadProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
     if (permissions.canManageProducts({ session })) {
       return true;
     }
-
     return { status: 'AVAILABLE' };
+  },
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+    return { id: session.itemId };
   },
 };
